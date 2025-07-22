26d14d28424d30afeec55106700c05086ff96ebd
pubnicaragua <clientesactivos4@gmail.com>
2025-07-21 21:23:24 -0600

    Fix JSX Syntax Error in PromocionesTodas - Final

--- src/components/Promociones/PromocionesTodas.tsx
+++ src/components/Promociones/PromocionesTodas.tsx
@@ -146,16 +146,6 @@ export function PromocionesTodas({ onShowModal }: PromocionesTodasProps) {
               Aplicar filtros
             </button>
           </div>
-        </div>
-      </Modal>
-          onClick={() => setShowFilters(true)}
-          className="flex items-center space-x-2 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
-        >
-          <Filter className="w-4 h-4" />
-          <span>Filtros</span>
-        </button>
-      </div>
-
       <div className="flex items-center justify-between">
         <div className="relative flex-1 max-w-md">
           <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
